98e0a762eb9da98787251aec90f749d991ffe2af
Pawel Kaczynski <pawel.kaczynski@digirati.co.uk>
2015-12-17 13:07:22 +0000

    Exceptions.Tests added. UT coverage for UnreachableCodeException.

---
+++ Gerino.Utils.Exceptions.Tests/BasicExceptionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Should;
+using Xunit;
+
+namespace Gerino.Utils.Exceptions.Tests
+{
+    public abstract class BasicExceptionTests<T> where T:Exception
+    {
+        [Fact]
+        public void NameEndsInException()
+        {
+            typeof (T).Name.EndsWith("Exception").ShouldBeTrue();
+        }
+
+        [Fact]
+        public void IsSerializable()
+        {
+            typeof(T).IsSerializable.ShouldBeTrue();
+        }
+    }
+}
---
+++ Gerino.Utils.Exceptions.Tests/UnreachableCodeExceptionTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Gerino.Utils.Exceptions;
+using Should;
+using Xunit;
+
+namespace Gerino.Utils.Exceptions.Tests
+{
+    public class UnreachableCodeExceptionTests : BasicExceptionTests<UnreachableCodeException>
+    {
+        [Fact]
+        public void HasParameterlessConstructor()
+        {
+            typeof (UnreachableCodeException).GetConstructor(Type.EmptyTypes).ShouldNotBeNull();
+        }
+    }
+}
